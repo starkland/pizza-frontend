e7f531cf4833a342477c7175ace42c71f39ea6d7
thulioph <thulioph@gmail.com>
2017-01-07 17:03:20 -0300

    Adiciona métodos para gerenciar status de conexão.

---
+++ js/offline.js
@@ -0,0 +1,28 @@
+(function() {
+
+  'use strict';
+
+  // ====
+
+  function handleStatus(event) {
+    var body, condition;
+
+    body = document.getElementsByTagName('body')[0];
+
+    switch(event.type) {
+      case 'offline':
+        body.classList.add('offline');
+      break;
+
+      case 'online':
+        body.classList.remove('offline');
+      break;
+    }
+  }
+
+  // ====
+
+  window.addEventListener('offline', handleStatus, false);
+  window.addEventListener('online', handleStatus, false);
+
+})();
