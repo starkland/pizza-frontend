4324a680b06ccf26916496dd7748e5c26df591d5
thulioph <thulioph@gmail.com>
2017-01-07 22:12:53 -0300

    Ajustes no módulo de about.

--- js/controllers/about.js
+++ js/controllers/about.js
@@ -2,7 +2,49 @@
   'use strict';
 
   function AboutCtrl() {
-    console.info('AboutCtrl');
+    var _video, playBtn, pauseBtn, Video;
+
+    _video = document.getElementsByTagName('video')[0];
+
+    playBtn = document.getElementById('playBtn');
+    pauseBtn = document.getElementById('pauseBtn');
+
+    // ====
+
+    Video = {
+      play: function() {
+        _video.play();
+
+        pauseBtn.classList.remove('js-active');
+        this.classList.add('js-active');
+      },
+
+      pause: function() {
+        _video.pause();
+
+        playBtn.classList.remove('js-active');
+        this.classList.add('js-active');
+      },
+
+      duration: function() {
+        var current, total, duracao;
+
+        current = Math.round(_video.currentTime);
+        total = Math.round(_video.duration);
+
+        duracao = document.getElementById('videoDuration');
+        duracao.innerHTML = total - current / 100;
+
+        window.requestAnimationFrame(Video.duration);
+      }
+    };
+
+    Video.duration();
+
+    // ====
+
+    playBtn.addEventListener('click', Video.play, false);
+    pauseBtn.addEventListener('click', Video.pause, false);
   }
 
   angular
